d04ba28e4c1b88224bbb1132a1ea579af3af47ac
Luat Nguyen <minhluet56@gmail.com>
2026-08-10 09:22:34 +0800

    Initial commit

---
+++ community-challenge/community-challenge/community_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  community_challengeApp.swift
+//  community-challenge
+//
+//  Created by Nguyen Minh Luat on 10/8/26.
+//
+
+import SwiftUI
+
+@main
+struct community_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
